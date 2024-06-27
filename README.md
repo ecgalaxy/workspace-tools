a48ad04dc16300c2fedf8c2f8762ea9f72f7138d
Jean-Francois HOVINNE <jean-francois.hovinne@ext.ec.europa.eu>
2024-06-27 15:34:58 +0000

    feat: Initial implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Workspace tools
+
+This Ansible playbook provides tools and automated configuration for developers' workspaces.
+
+## Requirements
+
+- Python 3.x with the `venv` module (`apt install python3-venv`), Ansible does *not* need to be installed.
+- The tools are installed system-wide, so administrative access is necessary.
+
+## Usage
+
+```
+sudo ./exec.sh
+```
---
+++ exec.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+WORKDIR=`dirname "$0"`
+python3 -m venv $WORKDIR/venv
+source $WORKDIR/venv/bin/activate
+pip install pip --upgrade
+pip install ansible --upgrade
+test -f $WORKDIR/requirements.yml && ansible-galaxy install -r $WORKDIR/requirements.yml -p $WORKDIR/roles
+ANSIBLE_REMOTE_TEMP=$WORKDIR/tmp ANSIBLE_ROLES_PATH=$WORKDIR/roles ansible-playbook -c local -i localhost, $WORKDIR/playbook.yml
+deactivate
+rm -rf $WORKDIR/roles $WORKDIR/tmp $WORKDIR/venv
