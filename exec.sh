d9cf9647c8362664df19abc4ba354fde16befad9
Jean-Francois HOVINNE <jean-francois.hovinne@ext.ec.europa.eu>
2024-09-19 14:26:45 +0000

    fix: Delegate cleanup tasks to caller (fixes #5)

--- exec.sh
+++ exec.sh
@@ -7,5 +7,3 @@ pip install pip --upgrade
 pip install ansible --upgrade
 test -f $WORKDIR/requirements.yml && ansible-galaxy install -r $WORKDIR/requirements.yml -p $WORKDIR/roles
 ANSIBLE_REMOTE_TEMP=$WORKDIR/tmp ANSIBLE_ROLES_PATH=$WORKDIR/roles ansible-playbook -c local -i localhost, $WORKDIR/playbook.yml
-deactivate
-rm -rf $WORKDIR/roles $WORKDIR/tmp $WORKDIR/venv
